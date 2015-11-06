aa0357dde599311ba65695310b4f7024f8d2541c
eric360 <eric-richard.hongtuanha@360learning.com>
2015-11-06 15:36:02 +0100

    Initial Commit

---
+++ Notes/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Notes
+//
+//  Created by eric_360 on 06/11/15.
+//  Copyright © 2015 Eric Hong tuan ha. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
